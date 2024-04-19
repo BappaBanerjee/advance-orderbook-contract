bd6629cfd507c430187d7259fdd4824fb8bd6324
BappaBanerjee <bappa3727@gmail.com>
2024-04-19 18:46:45 +0530

    last changes

--- deploy/01-orderbookFactory.deploy.js
+++ deploy/01-orderbookFactory.deploy.js
@@ -1,11 +1,21 @@
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("OrderbookFactory", {
+  const _contract = await deploy("OrderbookFactory", {
     from: deployer,
     args: [],
     log: true,
   });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(_contract.address, []);
+  }
 };
 
 module.exports.tags = ["all", "orderbookFactory"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
 require("@nomicfoundation/hardhat-ethers");
@@ -9,8 +9,38 @@ require("@nomicfoundation/hardhat-chai-matchers");
 
 require("hardhat-contract-sizer");
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 666666,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+  },
   namedAccounts: {
     deployer: {
       default: 0,
